f9b4cf0a4474ea45f07fea85fdda803c81956252
norrisollie <norrisollie@icloud.com>
2026-04-15 22:04:32 +0100

    feat: add new logo images, update logo component to use light version, and adjust header height in theme

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,12 +1,16 @@
+import logoLight from "../../assets/logo-light.png";
+
 function Logo() {
   console.log("Rendering component: logo");
 
   return (
     <div className="header__logo">
       <a href="#" className="no-underline">
-        Canvey
-        <br />
-        Foot Clinic
+        <img
+          src={logoLight}
+          alt="Canvey Foot Clinic Logo"
+          className="h-14.5 w-auto"
+        />
       </a>
     </div>
   );
